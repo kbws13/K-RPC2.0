b9c9905bec42c47daec2d5c89471777ca1028549
空白无上 <hsy040506@163.com>
2024-03-11 12:20:57 +0800

    添加 JDK 序列化

---
+++ k-rpc-simple/src/main/java/xyz/kbws/rpc/serializer/Serializer.java
@@ -0,0 +1,29 @@
+package xyz.kbws.rpc.serializer;
+
+import java.io.IOException;
+
+/**
+ * @author kbws
+ * @date 2024/3/11
+ * @description: 序列化接口
+ */
+public interface Serializer {
+    /**
+     * 序列化
+     * @param object
+     * @return
+     * @param <T>
+     * @throws IOException
+     */
+    <T> byte[] serialize(T object) throws IOException;
+
+    /**
+     * 反序列化
+     * @param bytes
+     * @param type
+     * @return
+     * @param <T>
+     * @throws IOException
+     */
+    <T> T deserialize(byte[] bytes, Class<T> type) throws IOException;
+}
