2926fdb9fb7182b87c13ad7b6e004e6d49a7efa9
空白无上 <hsy040506@163.com>
2024-03-13 09:31:33 +0800

    引入 Etcd 注册中心

---
+++ k-rpc-core/src/main/java/xyz/kbws/rpc/registry/Registry.java
@@ -0,0 +1,45 @@
+package xyz.kbws.rpc.registry;
+
+import xyz.kbws.rpc.config.RegistryConfig;
+import xyz.kbws.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author kbws
+ * @date 2024/3/13
+ * @description: 注册中心
+ */
+public interface Registry {
+
+    /**
+     * 初始化
+     * @param registryConfig
+     */
+    void init(RegistryConfig registryConfig);
+
+    /**
+     * 注册服务（服务端）
+     * @param serviceMetaInfo
+     */
+    void register(ServiceMetaInfo serviceMetaInfo) throws Exception;
+
+    /**
+     * 注销服务
+     * @param serviceMetaInfo
+     */
+    void unRegister(ServiceMetaInfo serviceMetaInfo);
+
+    /**
+     * 服务发现（客户端）
+     * @param serviceKey
+     * @return
+     */
+    List<ServiceMetaInfo> serviceDiscovery(String serviceKey);
+
+    /**
+     * 服务销毁
+     */
+    void destroy();
+}
