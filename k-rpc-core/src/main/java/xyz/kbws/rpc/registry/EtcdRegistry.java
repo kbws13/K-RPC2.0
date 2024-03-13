c96fe2f6f57db5defcd3ff5da6b0fa06f60be1f2
空白无上 <hsy040506@163.com>
2024-03-13 18:05:33 +0800

    添加缓存机制

--- k-rpc-core/src/main/java/xyz/kbws/rpc/registry/EtcdRegistry.java
+++ k-rpc-core/src/main/java/xyz/kbws/rpc/registry/EtcdRegistry.java
@@ -34,6 +34,11 @@ public class EtcdRegistry implements Registry{
     private final Set<String> localRegisterNodeKeySet = new HashSet<>();
 
     /**
+     * 注册中心服务缓存
+     */
+    private final RegistryServiceCache registryServiceCache = new RegistryServiceCache();
+
+    /**
      * 根节点
      */
     private static final String ETCD_ROOT_PATH = "/rpc/";
@@ -76,6 +81,12 @@ public class EtcdRegistry implements Registry{
 
     @Override
     public List<ServiceMetaInfo> serviceDiscovery(String serviceKey) {
+        // 优先从缓存获取服务
+        List<ServiceMetaInfo> cachedServiceMetaInfoList = registryServiceCache.readCache();
+        if (cachedServiceMetaInfoList != null) {
+            return cachedServiceMetaInfoList;
+        }
+
         // 前缀搜索，结尾一定要加 ‘/’
         String searchPrefix = ETCD_ROOT_PATH + serviceKey + "/";
         try {
@@ -87,12 +98,15 @@ public class EtcdRegistry implements Registry{
                     .get()
                     .getKvs();
             // 解析服务信息
-            return keyValues.stream()
+            List<ServiceMetaInfo> serviceMetaInfoList = keyValues.stream()
                     .map(keyValue -> {
                         String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
                         return JSONUtil.toBean(value, ServiceMetaInfo.class);
                     })
                     .collect(Collectors.toList());
+            // 写入服务缓存
+            registryServiceCache.writeCache(serviceMetaInfoList);
+            return serviceMetaInfoList;
         } catch (Exception e) {
             throw new RuntimeException("获取服务列表失败", e);
         }
@@ -101,6 +115,15 @@ public class EtcdRegistry implements Registry{
     @Override
     public void destroy() {
         System.out.println("当前节点下线");
+        // 下线节点
+        // 遍历本节点所有的 key
+        for (String key : localRegisterNodeKeySet) {
+            try {
+                kvClient.delete(ByteSequence.from(key, StandardCharsets.UTF_8));
+            } catch (Exception e) {
+                throw new RuntimeException(key + "节点下线失败");
+            }
+        }
         // 释放资源
         if (kvClient != null) {
             kvClient.close();
---
+++ k-rpc-core/src/main/java/xyz/kbws/rpc/registry/RegistryServiceCache.java
@@ -0,0 +1,40 @@
+package xyz.kbws.rpc.registry;
+
+import xyz.kbws.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * @author kbws
+ * @date 2024/3/13
+ * @description: 注册中心服务本地缓存
+ */
+public class RegistryServiceCache {
+    /**
+     * 服务缓存
+     */
+    List<ServiceMetaInfo> serviceCache;
+
+    /**
+     * 写缓存
+     * @param newServiceCache
+     */
+    void writeCache(List<ServiceMetaInfo> newServiceCache) {
+        this.serviceCache = newServiceCache;
+    }
+
+    /**
+     * 读缓存
+     * @return
+     */
+    List<ServiceMetaInfo> readCache() {
+        return this.serviceCache;
+    }
+
+    /**
+     * 清空缓存
+     */
+    void clearCache() {
+        this.serviceCache = null;
+    }
+}
