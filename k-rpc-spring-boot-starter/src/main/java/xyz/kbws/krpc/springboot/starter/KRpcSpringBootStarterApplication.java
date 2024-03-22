1dfee76139141c608a1d75cfe187251a48c26518
空白无上 <hsy040506@163.com>
2024-03-22 15:10:47 +0800

    SpringBoot-Starter 注解驱动

---
+++ k-rpc-spring-boot-starter/src/main/java/xyz/kbws/krpc/springboot/starter/KRpcSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package xyz.kbws.krpc.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KRpcSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KRpcSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ k-rpc-spring-boot-starter/src/test/java/xyz/kbws/krpcspringbootstarter/KRpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.kbws.krpcspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KRpcSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
